00121a7926e8054579dade9673f8e327cdd4e2f2
seekingtau <cameron@elda.bio>
2023-11-22 13:07:07 -0500

    new pages

--- app.py
+++ app.py
@@ -9,5 +9,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/contribute/')
+def contribute():
+    return render_template('contribute.html')
+
+@app.route('/partners/')
+def partners():
+    return render_template('partners.html')
+
+@app.route('/initiatives/')
+def initiatives():
+    return render_template('initiatives.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
